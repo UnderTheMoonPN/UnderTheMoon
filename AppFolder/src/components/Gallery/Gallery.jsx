b8495e9af31befbd874d19b888584c87eb4842cf
NicolaiSzuwalski <132432642+NicolaiSzuwalski@users.noreply.github.com>
2024-01-09 13:57:20 +0100

    galleri

--- AppFolder/src/components/Gallery/Gallery.jsx
+++ AppFolder/src/components/Gallery/Gallery.jsx
@@ -3,9 +3,9 @@ import style from './Gallery.module.scss'
 export const Gallery = props =>{
     console.log(props)
     return(
-    <div>    
-        Follow us on instagram
-        <div className='galleryContainer'>
+    <div className={style.follow}>    
+        <h2>Follow us on instagram</h2>
+        <div className={style.galleryContainer}>
             <div className={style.galleri}>
                 {props.pictures && props.pictures.map((picture, index)=>{
                     return(<img 
